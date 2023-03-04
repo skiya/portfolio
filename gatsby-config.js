a4a6ff4afe525aa8ef7dcd6a8b7dfa5163cdeba0
sqiye <30648722+sqiye@users.noreply.github.com>
2023-03-04 21:11:58 +0900

    add more details

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,18 @@ module.exports = {
     // Main Site Title
     title: `This is me.`,
     // Description that goes under your name in main bio
-    description: `Clean coder. Problem solver. Idea creator.`,
+    description: `Clean coder. Problem solver. Idea generator.`,
     // Optional: Github account URL
     github: `https://github.com/sqiye`,
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/%E5%A5%87%E5%86%B6-%E9%9A%8B-854997229/`,
     // Content of the About Me section
-    about: `I come from Dalian, China. Graduated from Dalian University of Technology with CS as my major, and Japanese as secondary degree. Been working as a Back-end engineer at Tokyo from 2020. When at work I am fully dedicated at finish my job with clean, manageable code and do my best in assisting my fellow co-workers. In private, I like to game with a sip of wine. Also, on vacations, I like traveling and enjoy wandering in different towns. I take "To boldly go where no one has gone before" from Star Trek as my motto, as I love to live on the bleeding edge of tech.`,
+    about: `I come from Dalian, China. Graduated from Dalian University of Technology with CS as my major, and Japanese as secondary degree. Been working as a Back-end engineer at Tokyo since 2020. When at work I am fully dedicated at finishing my job with clean & manageable code and do my best in assisting my fellow co-workers. In private, gaming with a sip of wine is my favorate. Also, on vacations, I like traveling and enjoy wandering in different towns. I take "To boldly go where no one has gone before" from Star Trek as my motto, as I love to live on the bleeding edge of tech.`,
     experience: [
       {
         name: 'teamLab Inc.',
         description: 'Backend Developer, October 2020 - Present',
+        link: 'https://www.team-lab.com/en/search/?q=Web%E3%82%B5%E3%82%A4%E3%83%88%E6%A7%8B%E7%AF%89'
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -25,16 +26,21 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'Kotlin, Spring Boot, Spring Cloud, JavaScript (ES6+), Typescript, Golang, Node.js, Express.js, React, Ruby on Rails, Dart, Flutter',
+          'Kotlin(2+ yrs), Spring Boot(3+ yrs), Spring Cloud, JavaScript(2+ yrs), Typescript, AWS SAM, Serverless, Golang, Node.js, Next.js, Ruby on Rails, Dart, Flutter',
       },
       {
         name: 'Databases',
         description: 'PostreSQL, MySQL, AWS DynamoDB',
       },
       {
+        name: 'Certifications',
+        description:
+          'AWS Solution Architect Associate, AWS Developer Associate',
+      },
+      {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), GCP, CI/CD (GitHub Actions, CodePipeline), API design, TDD',
+          'Docker, GCP, CI/CD (GitHub Actions, CodePipeline), API design, TDD',
       },
     ],
   },
